294832574452fa547aa722f04264959a6840b7d3
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2021-05-08 03:10:36 +0600

    update

--- main/views.py
+++ main/views.py
@@ -29,25 +29,34 @@ def register(request):
     if request.method == "POST":
         user_form = Register(request.POST)
         if user_form.is_valid():
-            user = user_form.save(commit=False)
-            user.is_active = False
-            user.save()
-            new_profile = Profile(user=user)
-            new_profile.save()
-            current_site = get_current_site(request)
-            mail_subject = 'Activate your blog account.'
-            message = render_to_string('acc_active_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
-            })
-            to_email = user_form.cleaned_data.get('email')
-            email = EmailMessage(
-                mail_subject, message, to=[to_email]
-            )
-            email.send()
-            return HttpResponse('Please confirm your email address to complete the registration')
+            email = user_form.cleaned_data['email']
+            get_email = User.objects.filter(email=email)
+            if email in get_email:
+                messages.warning(request, 'Email already exists.')
+                context = {
+                    'user_form': user_form
+                }
+                return render(request, 'register.html', context)
+            else:
+                user = user_form.save(commit=False)
+                user.is_active = False
+                user.save()
+                new_profile = Profile(user=user)
+                new_profile.save()
+                current_site = get_current_site(request)
+                mail_subject = 'Activate your blog account.'
+                message = render_to_string('acc_active_email.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': account_activation_token.make_token(user),
+                })
+                to_email = user_form.cleaned_data.get('email')
+                email = EmailMessage(
+                    mail_subject, message, to=[to_email]
+                )
+                email.send()
+                return HttpResponse('Please confirm your email address to complete the registration')
         else:
             context = {
                 'user_form': user_form
